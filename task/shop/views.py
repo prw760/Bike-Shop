2e9c5d83e7529d6dde8d293ce332718236a9af94
paulrwade <pau2237029@maricopa.edu>
2024-05-25 08:29:15 -0700

    Midstreat check-in of in-progress Stage 4 of Bike Shop project

--- task/shop/views.py
+++ task/shop/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponseRedirect
+from django.views import View
 from django.shortcuts import render
 from django.views import generic
+
+import views
 from .forms import BikeOrderForm
 from .models import Bike, Order, Basket
 
@@ -14,11 +17,11 @@ class BikeListView(generic.ListView):
 		return Bike.objects.all()
 
 
-class BikeDetailView(generic.DetailView):
+class BikeDetailView(generic.DetailView, generic.edit.FormMixin):
 
 	template_name = 'shop/bike_detail.html'  # Specify your own template name/location
-	form = BikeOrderForm
 	context_object_name = 'bike'  # your own name for the object as a template variable
+	form_class = BikeOrderForm
 
 	def get_queryset(self, *args, **kwargs):
 		bike_id = self.kwargs.get('pk')
@@ -27,6 +30,39 @@ class BikeDetailView(generic.DetailView):
 	def get_bike_quantity(self, *args, **kwargs):
 		bike_id = self.kwargs.get('pk')
 		return Bike.objects.get(id=bike_id).quantity
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['form'] = self.get_form()
+		return context
+
+	def post(self, request, *args, **kwargs):
+		form = self.get_form()
+		if form.is_valid():
+			return self.form_valid(form)
+		else:
+			return self.form_invalid(form)
+
+	def form_valid(self, form):
+		# This method is called when valid form data has been POSTed
+		# It should return an HttpResponse
+		return views.process_order(self.request)
+
+	def form_invalid(self, form):
+		# This method is called when invalid form data has been POSTed
+		# It should return an HttpResponse
+		return render(self.request, 'shop/bike_detail.html', {'form': form})
+
+	def get_success_url(self):
+		return f'/order/{Order.objects.last().id}/'
+
+	def get_initial(self):
+		initial = super().get_initial()
+		initial['bike_id'] = self.kwargs.get('pk')
+		return initial
+
+	def get(self, request, *args, **kwargs):
+		return super().get(request, *args, **kwargs)
 
 
 def process_order(request):
