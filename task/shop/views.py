bb33b6c4766f7e07e36ba2408882c654f4c48090
paulrwade <pau2237029@maricopa.edu>
2024-05-24 18:07:05 -0700

    Midstreat check-in of in-progress Stage 4 of Bike Shop project

--- task/shop/views.py
+++ task/shop/views.py
@@ -1,4 +1,5 @@
-from django.http import request, HttpResponseRedirect
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.views import generic
 from .forms import BikeOrderForm
 from .models import Bike, Order
@@ -16,6 +17,7 @@ class BikeListView(generic.ListView):
 class BikeDetailView(generic.DetailView):
 
 	template_name = 'shop/bike_detail.html'  # Specify your own template name/location
+	form = BikeOrderForm
 	context_object_name = 'bike'  # your own name for the object as a template variable
 
 	def get_queryset(self, *args, **kwargs):
@@ -26,36 +28,37 @@ class BikeDetailView(generic.DetailView):
 		bike_id = self.kwargs.get('pk')
 		return Bike.objects.get(id=bike_id).quantity
 
-	def form_post(self, *args, **kwargs):
+	def form_valid(self, *args, **kwargs):
 
-		if self.request.method == 'POST':
+		form = self.form
 
-			form = BikeOrderForm(self.request.POST)
+		name = form.cleaned_data['name']
+		surname = form.cleaned_data['surname']
+		phone_number = form.cleaned_data['phone_number']
+		bike_id = form.cleaned_data['bike_id']
 
-			if form.is_valid():
+		bike = Bike.objects.get(id=bike_id)
 
-				bike_id = form.cleaned_data['bike_id']
-				name = form.cleaned_data['name']
-				surname = form.cleaned_data['surname']
-				phone_number = form.cleaned_data['phone_number']
+		bike_order = Order.objects.create(bike=bike, name=name, surname=surname,
+											phone_number=phone_number, status='P')
+		bike_order.save()
 
-				bike = Bike.objects.get(id=bike_id)
+		bike.frame.quantity -= 1
+		bike.tires.quantity -= 1
+		bike.basket.quantity -= 1
+		bike.seat.quantity -= 1
+		# bike.quantity -= 1
 
-				bike_order = Order.objects.create(bike=bike, name=name, surname=surname,
-													phone_number=phone_number, status='P')
-				bike_order.save()
+		bike.save()
 
-				# bike.frame.quantity -= 1
-				# bike.tires.quantity -= 1
-				# bike.basket.quantity -= 1
-				# bike.seat.quantity -= 1
-				bike.quantity -= 1
-				bike.save()
-
-				return HttpResponseRedirect('/order/' + str(bike_order.id) + '/')
+		return HttpResponseRedirect("order/" + str(bike_order.id) + "/")
 
 
 class OrderView(generic.TemplateView):
 
 	template_name = 'shop/order.html'  # Specify your own template name/location
-	context_object_name = 'order'  # your own name for the object as a template variable
+
+
+
+
+
